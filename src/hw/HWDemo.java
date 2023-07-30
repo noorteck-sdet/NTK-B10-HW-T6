44465d02d8229764c931ac32db729ca6a034ff9e
masoo <masoo@192.168.1.170>
2023-07-30 18:42:52 -0500

    HW Attempt #1

--- src/hw/HWDemo.java
+++ src/hw/HWDemo.java
@@ -6,4 +6,10 @@ public class HWDemo {
 		System.out.println("HW");
 		System.out.println("Adel");
 	}
+	public static void getInfo(String [] args) {
+		
+		System.out.println("HW Attempt #1");
+		
+	}
+	
 }
