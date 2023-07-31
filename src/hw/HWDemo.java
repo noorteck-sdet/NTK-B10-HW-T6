70a9271dc443528a7e8623f465b885fad4fcf7ce
adelalkhalidy <140729981+adelalkhalidy@users.noreply.github.com>
2023-07-30 20:48:25 -0400

    Update HWDemo.java

--- src/hw/HWDemo.java
+++ src/hw/HWDemo.java
@@ -4,15 +4,12 @@ public class HWDemo {
 
 	public static void main(String[] args) {
 		System.out.println("HW");
-<<<<<<< HEAD.................................................................................................
+
 		System.out.println("Adel");
 		System.out.println("Java");
 		System.out.println("Java is Fun");
 		System.out.println("Adel & Masood are the best");
-=========================================================================================================================
-		System.out.println("Java is Fun");
-		System.out.println("working in team is fun")
-		System.ou.println("Today is Sunday");
+
 
 	}
 	
