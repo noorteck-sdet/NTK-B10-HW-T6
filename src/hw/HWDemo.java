c922b2a53a4d854409d7cc1ef4a60ec326482ecb
JRAAXX <140764985+JRAAXX@users.noreply.github.com>
2023-07-30 20:23:06 -0400

    Java

--- src/hw/HWDemo.java
+++ src/hw/HWDemo.java
@@ -4,5 +4,7 @@ public class HWDemo {
 
 	public static void main(String[] args) {
 		System.out.println("HW");
+		System.out.println("Java is Fun");
+		System.out.println("working in team is fun")
 	}
 }
