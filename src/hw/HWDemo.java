a6dcd62ef689b27e124164f5e5d7f08316929abe
JRAAXX <140764985+JRAAXX@users.noreply.github.com>
2023-07-30 20:27:47 -0400

    Update HWDemo.java

--- src/hw/HWDemo.java
+++ src/hw/HWDemo.java
@@ -6,6 +6,6 @@ public class HWDemo {
 		System.out.println("HW");
 		System.out.println("Java is Fun");
 		System.out.println("working in team is fun")
-			System.ou.println("Today is Sunday");
+		System.ou.println("Today is Sunday");
 	}
 }
