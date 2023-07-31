7338fcf91aca0f2ff98015e67dd4fce79fbf6fd7
ItsMaSooD <140644625+ItsMaSooD@users.noreply.github.com>
2023-07-30 19:55:25 -0500

    Update HWDemo.java

--- src/hw/HWDemo.java
+++ src/hw/HWDemo.java
@@ -4,16 +4,11 @@ public class HWDemo {
 
 	public static void main(String[] args) {
 		System.out.println("HW");
-<<<<<<< HEAD
 		System.out.println("Adel");
 		System.out.println("Java");
 		System.out.println("Java is Fun");
 		System.outprintln("Adel & Masoo are the best");
-=======
-		System.out.println("Java is Fun");
-		System.out.println("working in team is fun")
-		System.ou.println("Today is Sunday");
->>>>>>> branch 'master' of https://github.com/noorteck-sdet/NTK-B10-HW-T6.git
+
 	}
 	
 }
